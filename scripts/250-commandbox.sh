41e4dcae2597f2f3dbc9c80c7d0798afa99d6911
Sean Daniels <sean@braunsmedia.com>
2016-04-12 14:10:11 -0400

    sudo for keyserver

--- scripts/250-commandbox.sh
+++ scripts/250-commandbox.sh
@@ -4,7 +4,7 @@ echo "================= START 250-COMMANDBOX.SH ================="
 echo " "
 echo "BEGIN installing and configuring CommandBox ..."
 
-gpg --keyserver keys.gnupg.net --recv-key 6DA70622
-gpg -a --export 6DA70622 | sudo apt-key add -
+sudo gpg --keyserver keys.gnupg.net --recv-key 6DA70622
+sudo gpg -a --export 6DA70622 | sudo apt-key add -
 echo "deb http://downloads.ortussolutions.com/debs/noarch /" | sudo tee -a /etc/apt/sources.list.d/commandbox.list
 sudo apt-get update && sudo apt-get install commandbox
