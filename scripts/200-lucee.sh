6672821a8de24c378c09259f8b8129179c7c102a
Sean Daniels <sean@braunsmedia.com>
2016-06-07 08:02:41 -0400

    Remove personal FR instance
    This reverts commit e867899ddb52f42a699bcb1441b5f08199ad0c9d.
    
    # Conflicts:
    #	scripts/200-lucee.sh

--- scripts/200-lucee.sh
+++ scripts/200-lucee.sh
@@ -26,17 +26,6 @@ cp etc/lucee/server.xml /opt/lucee/tomcat/conf
 
 sudo chown deploy:deploy /opt/lucee/tomcat/conf/server.xml
 
-if [ ! -d "/opt/fusionreactor" ]; then
-	echo "... Installing FusionReactor ..."
-	sudo cp -R etc/fr /opt/fusionreactor
-	sudo ln -s /opt/fusionreactor/etc/lib/fusionreactor.jar /opt/lucee/tomcat/lib
-	sudo ln -s /opt/fusionreactor/etc/lib/libFusionReactor-linux-x86_64.so /usr/lib
-	sudo cp -f etc/lucee/web.xml /opt/lucee/tomcat/conf/web.xml
-	
-	sudo chown deploy:deploy /opt/lucee/tomcat/conf/web.xml
-	sudo chown deploy:deploy /opt/fusionreactor -R
-fi
-
 echo "... Installing MongoDB Extension ..."
 sudo cp -R etc/lucee/mongodb-extension-modern/* /opt/lucee/tomcat/lucee-server/deploy/
 
